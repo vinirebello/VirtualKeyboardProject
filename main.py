ea880b6c671103ad5ac9d7d7e6dfa635cab1b57e
vinirebello <v.rebello21@gmail.com>
2025-03-09 17:36:03 -0300

    added get user password function

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from pymongo import MongoClient
 from fastapi import FastAPI, HTTPException
 import random
+from fastapi.middleware.cors import CORSMiddleware
 import hashlib
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 connectionString = 'mongodb+srv://viniadmin:54321@cluster0.u7owh.mongodb.net/'
 client = MongoClient(connectionString)
@@ -19,31 +28,72 @@ user = {
 
 # collectionUsers.insert_one(user)
 
-def generateCombinations():
-    combinationsList = []
+@app.get("/getCombination")
+async def randomCombination():
+    total_combinations = collectionCombinations.count_documents({})
+    
+    if total_combinations == 0:
+        raise HTTPException(status_code=404, detail="Nenhuma combinação encontrada no banco")
 
-    while len(combinationsList) < 100:
-        numbers = list(range(10))
-        random.shuffle(numbers)
+    random_index = random.randint(0, total_combinations - 1)
+    combination = collectionCombinations.find().skip(random_index).limit(1)[0]
 
-        valid_combinations = []
-        i = 0
+    return {"hash": combination["hash"], "combination": combination["combination"]}
 
-        while i < len(numbers) - 1:
-            if abs(numbers[i] - numbers[i + 1]) > 1:  
-                valid_combinations.append([numbers[i], numbers[i + 1]])
-                i += 2 
-            else:
-                random.shuffle(numbers)  
-                valid_combinations = []  
-                i = 0
+@app.get("/getUserPassword")
+async def getPassword():
+    user = collectionUsers.find_one()
 
-        if len(valid_combinations) == 5: 
-            combinationsList.append(valid_combinations)
+    userPassword = user["senha"]
 
-    return combinationsList
+    return userPassword
 
-print(generateCombinations())
+@app.post("/passwordValidation")
+def validar_senha(payload: dict):
+
+    user_id = payload.get("_id")
+    senha_digitada = payload.get("senha")
+
+    user = collectionUsers.find_one({"id": user_id})
+
+    if not user:
+        raise HTTPException(status_code=404, detail="Usuário não encontrado")
+
+    userPassword = user["senha"]
+
+    if senha_digitada == userPassword:
+        return {"success": True, "message": "Senha correta!"}
+    
+    
+    nova_combinacao = randomCombination()
+    return {"success": False, "message": "Senha incorreta!", "nova_combinacao": nova_combinacao}
+
+
+# def generateCombinations():
+#     combinationsList = []
+
+#     while len(combinationsList) < 100:
+#         numbers = list(range(10))
+#         random.shuffle(numbers)
+
+#         valid_combinations = []
+#         i = 0
+
+#         while i < len(numbers) - 1:
+#             if abs(numbers[i] - numbers[i + 1]) > 1:  
+#                 valid_combinations.append([numbers[i], numbers[i + 1]])
+#                 i += 2 
+#             else:
+#                 random.shuffle(numbers)  
+#                 valid_combinations = []  
+#                 i = 0
+
+#         if len(valid_combinations) == 5: 
+#             combinationsList.append(valid_combinations)
+
+#     return combinationsList
+
+# print(generateCombinations())
 
 # def insert_db(combination):
 #     combination_str = str(combination) 
@@ -60,17 +110,3 @@ print(generateCombinations())
 
 # for combination in combinations:
 #     insert_db(combination)
-
-# print(colletionCombinations)
-
-@app.get("/newCombination")
-def randomCombination():
-    total_combinations = collectionCombinations.count_documents({})
-    
-    if total_combinations == 0:
-        raise HTTPException(status_code=404, detail="Nenhuma combinação encontrada no banco")
-
-    random_index = random.randint(0, total_combinations - 1)
-    combination = collectionCombinations.find().skip(random_index).limit(1)[0]
-
-    return {"hash": combination["hash"], "combination": combination["combination"]}
